584692f569e05a09e31c996db09cbb872f6cf2aa
Your Name <iamvikasbareth@gmail.com>
2023-08-03 00:31:09 +0530

    added js clear function

---
+++ index.js
@@ -0,0 +1,25 @@
+const boxes = document.querySelectorAll('.box');
+const gameInfo = document.querySelector(".game-info");
+const newGameBtn = document.querySelector(".btn");
+
+let currentPlayer;
+let gameGrid;
+const winningPositions = [
+        [0,1,2],[3,4,5],[6,7,8],
+        [0,3,6],[1,4,7],[2,5,8],
+        [0,4,8],
+        [2,4,6]
+        ]
+
+function initGame(){
+    currentPlayer = 'x';
+    gameGrid = ["","","","","","","","",""];
+    newGameBtn.classList.remove("active");
+    gameInfo.innerText = `Current Player - ${currentPlayer}`;
+}
+
+boxes.forEach((box,index) => {
+    box.addEventListener("click", ()=>{
+        handleClick(index)
+    })
+})
